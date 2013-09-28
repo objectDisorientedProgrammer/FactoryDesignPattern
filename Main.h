ceaeb018cadc150eb36b9a6e3aca22bd198a0be5
Doug <tikiman92@gmail.com>
2013-09-28 15:25:27 -0700

    Create Main.h

---
+++ Main.h
@@ -0,0 +1,14 @@
+/*
+	Main.h
+	Created September 28, 2013
+	Author: Douglas Chidester
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+using namespace std;
+#include <iostream>
+#include "Factory.h"
+
+#endif
